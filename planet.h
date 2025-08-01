8abe9639d961738257dfa462c3b4a1b7a0f7aa71
Nicholas <nap44794@uga.edu>
2025-07-31 22:34:56 -0400

    Refactor: remove extra get/set methods

--- planet.h
+++ planet.h
@@ -15,24 +15,10 @@ class Planet
     public:
         void makePlanet();
 
-        int getMass();
-        void setMass(int mass);
-
-        int getRadius();
-        void setRadius(int radius);
-        
-        sf::Vector2<double> getPosition();
-        void setPosition(double x, double y);
-        
-        sf::Vector2<double> getVelocity();
-        void setVelocity(double x, double y);
-        
-        sf::Vector2<double> getAcceleration();
-        void setAcceleration(double x, double y);
-
     private:
         int mass;
         int radius;
+        double netForce;
         sf::Vector2<double> position;
         sf::Vector2<double> velocity;
         sf::Vector2<double> acceleration;
